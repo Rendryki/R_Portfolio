3d78502297b0bcffcabdbbe0606f51990b9f176f
Rendryki Melo <rendrykifelipesilva@gmail.com>
2022-12-13 14:16:30 -0300

    att animações

--- Assets/pageEvents.js
+++ Assets/pageEvents.js
@@ -1,2 +1,24 @@
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+        })
+
+const item = document.querySelectorAll("[data-animation]");
+const animeScroll = () => {
+const windowTop = window.pageYOffset + window.innerHeight * 0.75 ;
+
+item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+    element.classList.add("animate");
+    } else {
+    element.classList.remove("animate");
+    }
+});
+};
+
+window.addEventListener("scroll", ()=>{
+animeScroll();
+})
+
 
 
